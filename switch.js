59488201c08f58804387603325f14c622dc30f8e
md-omarfaruk <87226097+md-omarfaruk@users.noreply.github.com>
2021-10-28 09:03:58 +0600

    switch-js
    switch-js

---
+++ switch.js
@@ -0,0 +1,36 @@
+var friends = 7;
+switch(friends){
+    case 10:
+        console.log('no i have not 10');
+        break;
+    case 9:
+        console.log('no i have not 9');
+        break;
+    case 11:
+    case 12:
+        console.log('no i have not 11 and 12');
+        break;
+    case 7:
+        console.log('yes, i have');
+        break;
+    default:
+        console.log('pai nai');
+}
+
+var friends = 9;
+switch(friends){
+    case 3:
+        console.log("no it's not 3");
+        break;
+    case 5:
+        console.log("no it's not 5");
+        break;
+    case 7:
+        console.log("no it's not 7");
+        break;
+    case 11:
+        console.log("no it's not 11");
+        break;
+    default:
+        console.log("sorry i didn't find 9");
+}
